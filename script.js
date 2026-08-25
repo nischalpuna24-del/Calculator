7ef7675647dfcc8decd24039cc115de829414aa9
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-25 09:58:28 +0545

    Complete calculator

---
+++ script.js
@@ -0,0 +1,62 @@
+let string = "";
+let memory = 0;
+
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button) => {
+
+    button.addEventListener('click', (e) => {
+
+        if (e.target.innerHTML == '=') {
+
+            string = eval(string);
+
+            document.querySelector('input').value = string;
+
+        }
+
+        else if (e.target.innerHTML == 'C') {
+
+            string = "";
+
+            document.querySelector('input').value = string;
+
+        }
+
+        else if (e.target.innerHTML == 'M+') {
+
+            memory = memory + Number(string);
+
+            string = "";
+
+            document.querySelector('input').value = "";
+
+            console.log("Memory:", memory);
+
+        }
+
+        else if (e.target.innerHTML == 'M-') {
+
+            memory = memory - Number(string);
+
+            string = "";
+
+            document.querySelector('input').value = "";
+
+            console.log("Memory:", memory);
+
+        }
+
+        else {
+
+            console.log(e.target);
+
+            string = string + e.target.innerHTML;
+
+            document.querySelector('input').value = string;
+
+        }
+
+    });
+
+});
